2b728d64a0e382790f22d574023f0a6c51cae1ed
Zhangir Azerbayev <zazerbayev@gmail.com>
2021-11-03 02:12:10 -0400

    fully working few shot evaluation including pass@k

--- evaluate_mathqapython.py
+++ evaluate_mathqapython.py
@@ -32,6 +32,8 @@ data_path = cfg['data_path']
 model_path = cfg['model_path']
 param_count = cfg['param_count']
 device = cfg['device']
+pass_at = cfg['pass_at']
+temp = cfg['temp']
 
 os.mkdir("results/" + experiment_name)
 
@@ -68,6 +70,7 @@ else:
 print("doing evaluation loop")
 num_correct = 0 
 no_errors = 0 
+sum_passk = 0 
 for batch in tqdm(loader): 
     input_ids, mask, code_sol, answer_sol = batch 
 
@@ -106,46 +109,66 @@ for batch in tqdm(loader):
     generated_ids = model.generate(
         input_ids=full_ids.long(), 
         do_sample=True, 
-        temperature=0.2, 
+        temperature=temp, 
         max_new_tokens=256, 
-        pad_token_id=tokenizer.eos_token_id
+        pad_token_id=tokenizer.eos_token_id, 
+        num_return_sequences=pass_at
     )
 
-    # Isolate one program completion 
+    # Does pass@k
+    correct_per_sample = 0 
+    correct_program = None 
     start_idx = encoded_few_shot_prompt.size()[1]
-    completion = tokenizer.decode(generated_ids[0, start_idx:])
-    answer_locs = re.search('answer.*?\n', completion)
-    if answer_locs: 
-        program = completion[:answer_locs.span()[1]]
+    
+    
+    for sample in generated_ids: 
+        completion = tokenizer.decode(sample[start_idx:])
+        answer_locs = re.search('answer.*?\n', completion)
+        if answer_locs: 
+            program = completion[:answer_locs.span()[1]]
+        else: 
+            program = completion 
+        answer = semisafe_evaluate(program, 'answer', 1)
+        if isinstance(answer, float): 
+            if abs((answer - answer_sol) / answer) < 0.01: 
+                correct_per_sample += 1
+                correct_program = program 
+                answer_to_save = answer 
+            no_errors += 1
+
+    if correct_program: 
+        program_to_save = correct_program
+        num_correct += 1
     else: 
-        program = completion 
-    answer = semisafe_evaluate(program, 'answer', 1)
-    if answer is float: 
-        if abs((answer - answer_sol) / answer) < 0.01: 
-            num_correct += 1
-        no_errors += 1
+        program_to_save = program # Effectively, just a sample 
+        answer_to_save = answer 
+
+    passk = float(correct_per_sample)/pass_at 
+    sum_passk += passk 
 
     # Writes results to a file
     to_dump += "\n" + "#"*20 + "\n"
     to_dump += "PROMPT: \n"
     to_dump += tokenizer.decode(full_ids.squeeze(), skip_special_tokens=True)
     to_dump += "\n\nGENERATED COMPLETION: \n" 
-    to_dump += completion
+    to_dump += program_to_save 
     to_dump += "\n\nLABEL COMPLETION:\n"
     to_dump += tokenizer.decode(code_sol.squeeze(), skip_special_tokens=True)
-    to_dump += "\n\nANSWER: " + str(answer) + "\n"
-    to_dump += "\nLABEL ANSWER: " + str(answer_sol.item()) + "\n"
+    to_dump += "\n\nANSWER: " + str(answer_to_save) + "\n"
+    to_dump += "\nLABEL ANSWER: " + str(answer_sol.item()) 
+    to_dump += "\n\nPASS@K: " + str(float(correct_per_sample)/pass_at) + "\n"
 
-    break 
 
 
     
 accuracy = num_correct / len(data) 
-execution_rate = no_errors / len(data)
+execution_rate = no_errors / (len(data)*pass_at)
+avg_passk = sum_passk/len(data)
 
 metrics = {
         "accuracy": accuracy, 
-        "execution_rate": execution_rate
+        "execution_rate": execution_rate, 
+        "avg_pass@k": avg_passk
         }
 
 with open("results/" + experiment_name + "/metrics.txt", "w") as fle: 
